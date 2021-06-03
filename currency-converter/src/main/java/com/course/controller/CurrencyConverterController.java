d4103ce08a07f6cb8bd6c972c740a8eb975a67bf
leonik <leonov.pegas@gmail.com>
2021-06-03 19:04:42 +0300

    Done with config server

--- currency-converter/src/main/java/com/course/controller/CurrencyConverterController.java
+++ currency-converter/src/main/java/com/course/controller/CurrencyConverterController.java
@@ -1,5 +1,7 @@
 package com.course.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/currency-converter")
 public class CurrencyConverterController {
 
+    @Value("${my.property: default value}")
+    private String fromConfig;
 
-
+    @GetMapping
+    public String getStringFromConfigServer() {
+        return "hello, it's your response: " + fromConfig;
+    }
 
 }
